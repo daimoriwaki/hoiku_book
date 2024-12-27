e8c59983326ea13f8010649e31d3f73c55d0efa4
daimoriwaki <daisuke.moriwaki@gmail.com>
2024-12-28 08:27:56 +0900

    add ch5

---
+++ ch5/code/5_1.R
@@ -0,0 +1,39 @@
+# 5-1
+# シミュレーションデータ
+
+
+# Load the necessary libraries
+library(ggplot2)
+library(rstudioapi)
+
+script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+
+# Set the working directory to the script's directory
+setwd(script_dir)
+
+# Load the CSV file using the relative path load ../data/5_1_sim.xslx sheet グラフ用データ
+
+data <- readxl::read_excel("../data/5_1_sim.xlsx", sheet = "グラフ用データ")
+
+# plot x axis  "きょうだいへの加点"   y axis  "きょうだいありの入所率" and "きょうだい同時申込の入所率"
+# add 180 label at x axis
+ggplot(data, aes(x = `きょうだいへの加点`)) +
+  geom_line(aes(y = `きょうだいなしの入所率` * 100, linetype = "きょうだいなしの入所率")) +
+  geom_line(aes(y = `きょうだい同時申込の入所率` * 100, linetype = "きょうだい同時申込の入所率")) +
+  geom_vline(xintercept = 180, linetype = "dotted") +
+  scale_x_continuous(breaks = c(180, scales::pretty_breaks()(data$`きょうだいへの加点`))) +
+  scale_linetype_manual(
+    values = c("きょうだいなしの入所率" = "solid", "きょうだい同時申込の入所率" = "dashed"),
+    name = "入所率"
+  ) +
+  theme_minimal(base_family = "HiraKakuPro-W3") +
+  theme(
+    legend.position = "None",
+    axis.text.x = element_text(angle = 0, hjust = 0.5, family = "HiraKakuProN-W3"),
+    strip.text = element_text(size = 10), # Adjust facet labels for better visibility
+    plot.margin = margin(20, 20, 20, 20) # Add consistent padding around the plot
+  ) +
+  labs(
+    x = "きょうだいへの加点",
+    y = "入所率 (%)"
+  )
---
+++ ch5/code/5_2.R
@@ -0,0 +1,42 @@
+# 5-2
+
+
+# Load the necessary libraries
+library(ggplot2)
+library(dplyr)
+library(rstudioapi)
+library(tidyr)
+
+script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+
+# Set the working directory to the script's directory
+setwd(script_dir)
+
+
+data <- readxl::read_excel("../data/5_2.xlsx", sheet = "Sheet1")
+
+
+# Step 1: Pivot data to long format
+data_long <- data %>%
+  pivot_longer(cols = -color_label, names_to = "year", values_to = "share") %>%
+  mutate(year = as.numeric(year))  # Convert year to numeric
+
+# Step 2: Rename columns
+
+data_long <- data_long %>%
+  rename(年 = year, 区分 = color_label, 入所率 = share)
+
+# Step 3: Filter for selected categories and convert to percentages
+data_filtered <- data_long %>%
+  filter(区分 %in% c("きょうだい無し", "きょうだい同時申込")) %>%
+  mutate(入所率 = 入所率 * 100)  # Convert to percentages
+
+# Step 4: Create line chart with percentage labels
+ggplot(data_filtered, aes(x = 年, y = 入所率, group = 区分, color = 区分)) +
+  geom_line(aes(linetype = 区分), size = 1) +
+  geom_point(size = 2) +
+  geom_text(aes(label = paste0(round(入所率, 1), "%")), vjust = -1, hjust = 1.5, size = 3) + # Add % symbol to labels
+  scale_color_grey(start = 0.3, end = 0.5) +
+  theme_minimal(base_family = "HiraKakuPro-W3") +
+  labs(title = "入所率の推移", x = "年", y = "入所率（%）") +
+  theme(legend.position = "none") 
